bc4747c874ef5fadc5e0ac9287053d0329ea3e37
Henrique Ramos Da Costa - FATEC - Cuiaba/MT <00590072@alunosenai.mt>
2025-10-23 21:28:16 -0400

    Criação da tela de login

---
+++ app/login.tsx
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from "react-native";
+
+export default function Login(){
+  return(
+    <View style={{Style.container}}>
+ 
+       <Text>
+      
+       Só alegria
+    
+       </Text>
+
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+      container:{
+        flex:1,
+      }
+
+});
